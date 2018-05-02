5f5f85c000b928b506c965f1fab0c48257488abd
Dr3wbit <eightbitandrew@gmail.com>
2018-05-02 16:18:21 -0700

    todo list displayed

---
+++ src/helpers/list_data.js
@@ -0,0 +1,20 @@
+
+export default [
+    {
+        title: 'Learn React',
+        details: 'Learn ReactJS'
+    },
+    {
+        title: 'Kill Keith',
+        details: 'Use spork'
+    },
+    {
+        title: 'Make Corey Happy',
+        details: 'You know *wink*'
+    },
+    {
+        title: 'Go to the Bathroom',
+        details: 'number 2'
+    }
+    
+];
