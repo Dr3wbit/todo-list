e2932e17200ed318303ad753c3e72d81a885eace
Dr3wbit <eightbitandrew@gmail.com>
2018-05-04 17:41:50 -0700

    connected to sever

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,17 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import '../assets/css/app.css';
 import React, { Component } from 'react';
+import axios from 'axios';
 import List from './list';
 import AddItem from './add-items';
 import listData from '../helpers/list_data';
 
+const BASE_URL = 'http://api.reactprototypes.com';
+const API_KEY = '?key=poopfarts';
+
 
 class App extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -15,36 +19,43 @@ class App extends Component {
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.getListData();
     }
-    
-    addItem(item){
-        this.setState({
-            listData: [item, ...this.state.listData]
-        });
+
+    async addItem(item) {
+        await axios.post(`${BASE_URL}/todos${API_KEY}`, item);
+
+        this.getListData();
     }
 
-    getListData(){
-        setTimeout(() => {
-            this.setState({ listData: listData });
-        }, 500)
+    async getListData() {
+        try {
+            const response = await axios.get(`${BASE_URL}/todos${API_KEY}`);
+
+            this.setState({
+                listData: response.data.todos
+            });
+            console.log(response.data.todos)
+        } catch (error) {
+            console.log('ERROR:', error.message);
+        }
     }
 
-    deleteItem(index){
-        const listData = this.state.listData.slice();
 
-        listData.splice(index, 1);
 
-        this.setState({ listData: listData});
+    async deleteItem(id) {
 
+        await axios.delete(`${BASE_URL}/todos/${id + API_KEY}`);
+
+        this.getListData();
     }
 
     render() {
         return (
             <div className="container">
                 <h1 className="center">To Do List</h1>
-                <AddItem add={this.addItem.bind(this)}/>
+                <AddItem add={this.addItem.bind(this)} />
                 <List data={this.state.listData} delete={this.deleteItem.bind(this)} />
             </div>
         );
